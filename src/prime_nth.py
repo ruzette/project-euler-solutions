297a7f7280dd67c46356c33b8c837dc673fc2d36
ruzette <ruzette.tanyag@gmail.com>
2014-11-28 11:23:57 -0500

    Added description

--- src/prime_nth.py
+++ src/prime_nth.py
@@ -1,8 +1,10 @@
 # Project Euler Problem 7 
 #
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # 
+# What is the 10 001st prime number?
 # 
-# Link : 
+# Link : https://projecteuler.net/problem=7 
 # 
 
 def isPrime(n, prime_list):
